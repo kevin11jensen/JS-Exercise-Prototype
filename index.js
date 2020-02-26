edeb5ebb7fd92b36af3793b3f8c16371a45f3f12
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-02-26 11:47:22 -0800

    done with mvp, working on stretch

--- index.js
+++ index.js
@@ -39,20 +39,20 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person(name, age) {
+function Person(name, age){
   this.stomach = [];
   this.name = name;
   this.age = age;
 }
-Person.prototype.eat = function(edible) {
-  if (this.stomach.length < 10) {
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
     this.stomach.push(edible);
   }
 };
-Person.prototype.poop = function() {
-  this.stomach = [];
+Person.prototype.poop = function(){
+  this.stomach =[];
 };
-Person.prototype.toString = function() {
+Person.prototype.toString = function(){
   return `${this.name}, ${this.age}`;
 };
 
@@ -70,15 +70,18 @@ Person.prototype.toString = function() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car(model, milesPerGallon) {
+function Car(model, milesPerGallon){
   this.model = model;
   this.milesPerGallon = milesPerGallon;
   this.tank = 0;
   this.odometer = 0;
 }
-Car.prototype.fill = function(gallons) {
+Car.prototype.fill = function(gallons){
   return (this.tank = this.tank + gallons);
 };
+Car.prototype.drive = function(){
+
+}
 
 /*
   TASK 3
@@ -87,18 +90,15 @@ Car.prototype.fill = function(gallons) {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby(name, age, favoriteToy) {
+function Baby(name, age, favoriteToy){
   Person.call(this, name, age);
   this.favoriteToy = favoriteToy;
 }
-
 Baby.prototype = Object.create(Person.prototype);
 
-Baby.prototype = Object.create(Person.prototype);
-
-Baby.prototype.play = function() {
-  return `Playing with ${this.favoriteToy}`;
-};
+Baby.prototype.play = function(){
+  return (`Playing with ${this.favoriteToy}`);
+}
 
 /* 
   TASK 4
